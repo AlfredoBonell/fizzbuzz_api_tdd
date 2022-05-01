816de7fc6173946fe1c6ca333cb23f34eff44ef2
Alfredo Bonell <alfredobonell96@gmail.com>
2022-05-01 13:29:42 -0600

    Carga inicial

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,18 @@
+const ExplorerService = require("./../../lib/services/ExplorerService");
+
+describe("Tests para ExplorerService", () => {
+    test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
+        const explorers = [{mission: "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
+    });
+
+    test("Requerimiento 2: Retornar todos los explorers que contengan cierta tecnologia" , () => {
+        const explorers = [{stacks: ["javascript"]} ,{stacks: ["elixir"]} , {stacks: ["java"]} , {stacks: ["javascript"]} ];
+        const explorersJavascriptInStack = ExplorerService.getExplorersByTechnology(explorers , "javascript");
+        explorersJavascriptInStack.forEach( explorer => {
+            expect(explorer.stacks).toContain("javascript");
+        });
+        expect(explorersJavascriptInStack.length).toEqual(2);
+    });
+});
